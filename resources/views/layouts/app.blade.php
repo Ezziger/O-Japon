8f34f342c3ef323b9615a26bc748c5a63a8e3c93
Ezziger <84440513+Ezziger@users.noreply.github.com>
2021-11-10 16:26:52 +0100

    css modification 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
-    <link href="./css/app.css" rel="stylesheet">
-    <script type="text/javascript" src="./js/script.js" defer></script>
+    <link href="/css/app.css" rel="stylesheet">
+    <script type="text/javascript" src="/js/script.js" defer></script>
 </head>
 
 <body>
